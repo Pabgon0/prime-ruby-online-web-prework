efb0bf99596470d45c6284c7a9d55659620fa6ba
Pablo Gonzalez <pabgon0@gmail.com>
2019-05-09 23:07:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,9 @@ def prime?(int)
   if int <= 1
     false
   end
-  for x in (2..int - 1)
-    if int % n == 0
+  (2..int-1).each do |x|
+    if number % x == 0
       false
     end
-  end
   true
 end
