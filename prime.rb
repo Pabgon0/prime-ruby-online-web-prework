95e62c33390a4d7004c324e2eb57931278650b78
Pablo Gonzalez <pabgon0@gmail.com>
2019-05-09 23:14:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,11 @@
 def prime?(int)
   if int < 2
     false
-  elsif
-  (2..int-1).each do |x|
-    if (int%x) == 0
+  end
+  (2..int - 1).each do |x|
+    if (int % x) == 0
       false
     end
-  else
-    true
   end
+  true
 end
