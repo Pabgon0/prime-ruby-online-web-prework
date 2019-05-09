2609fd463967c2358598c591f1efcabefb2ef096
Pablo Gonzalez <pabgon0@gmail.com>
2019-05-09 23:04:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 def prime?(int)
   if int <= 1
     false
-  else
-    for x in (2..int - 1)
-      if int % n == 0
-        false
-      end
+  end
+  for x in (2..int - 1)
+    if int % n == 0
+      false
+    end
   end
   true
 end
